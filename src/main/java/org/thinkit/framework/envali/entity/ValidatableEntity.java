eb85d15c7331ecdab86e464a0c840576e9ea6aa3
Kato Shinya <yourdream28@gmail.com>
2020-11-01 10:19:18 +0900

    add

---
+++ src/main/java/org/thinkit/framework/envali/entity/ValidatableEntity.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.framework.envali.entity;
+
+/**
+ * A marker interface that indicates that it is a verifiable entity.
+ * <p>
+ * There are no specific operations defined for this marker interface, only that
+ * the class that implements it is a verifiable entity.
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+public interface ValidatableEntity {
+}
