4475ef47b069c9e8bc97aea1cf1d208ce64a1fe0
Kato Shinya <yourdream28@gmail.com>
2020-11-02 23:21:52 +0900

    add

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.thinkit.framework.envali.annotation","c":"ParameterMapping","l":"content()"},{"p":"org.thinkit.framework.envali","c":"Envali","l":"validate(ValidatableEntity)","u":"validate(org.thinkit.framework.envali.entity.ValidatableEntity)"}];updateSearchResults();
---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.thinkit.framework.envali"},{"l":"org.thinkit.framework.envali.annotation"},{"l":"org.thinkit.framework.envali.entity"}];updateSearchResults();
