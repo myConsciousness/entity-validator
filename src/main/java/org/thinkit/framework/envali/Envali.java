2dd2af6e330e87f7eb9e83c34184dd1fffa6d84c
Kato Shinya <yourdream28@gmail.com>
2020-11-04 14:37:58 +0900

    improved data structure

--- src/main/java/org/thinkit/framework/envali/Envali.java
+++ src/main/java/org/thinkit/framework/envali/Envali.java
@@ -14,16 +14,9 @@
 
 package org.thinkit.framework.envali;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.thinkit.common.Preconditions;
-import org.thinkit.common.catalog.Extension;
-import org.thinkit.framework.content.ContentLoader;
 import org.thinkit.framework.envali.annotation.ParameterMapping;
 import org.thinkit.framework.envali.annotation.RequireEndWith;
 import org.thinkit.framework.envali.annotation.RequireNegative;
@@ -34,11 +27,9 @@ import org.thinkit.framework.envali.annotation.RequirePositive;
 import org.thinkit.framework.envali.annotation.RequireRangeFromTo;
 import org.thinkit.framework.envali.annotation.RequireRangeTo;
 import org.thinkit.framework.envali.annotation.RequireStartWith;
-import org.thinkit.framework.envali.catalog.EnvaliContentAttribute;
-import org.thinkit.framework.envali.catalog.EnvaliContentCondition;
-import org.thinkit.framework.envali.catalog.EnvaliContentRoot;
 import org.thinkit.framework.envali.entity.ValidatableEntity;
 import org.thinkit.framework.envali.exception.InvalidValueDetectedException;
+import org.thinkit.framework.envali.strategy.AnnotationContext;
 
 /**
  * {@link Envali} is a powerful interface that provides common and intuitive
@@ -126,179 +117,13 @@ public interface Envali {
         Preconditions.requireNonNull(entity);
 
         final Class<? extends ValidatableEntity> entityClass = entity.getClass();
-        final ParameterMapping contentMapping = entityClass.getAnnotation(ParameterMapping.class);
 
         Arrays.asList(entityClass.getDeclaredFields()).forEach(field -> {
             field.setAccessible(true);
 
             Arrays.asList(field.getAnnotations()).forEach(annotation -> {
-                try {
-                    final Class<? extends Annotation> annotationType = annotation.annotationType();
-
-                    if (annotationType.equals(RequireNonNull.class)) {
-                        Preconditions.requireNonNull(field.get(entity));
-                    } else if (annotationType.equals(RequireNonBlank.class)) {
-                        Preconditions.requireNonBlank(getString(entity, field), new InvalidValueDetectedException());
-                    } else if (annotationType.equals(RequirePositive.class)) {
-                        Preconditions.requirePositive(getInt(entity, field), new InvalidValueDetectedException());
-                    } else if (annotationType.equals(RequireNegative.class)) {
-                        Preconditions.requireNegative(getInt(entity, field), new InvalidValueDetectedException());
-                    } else if (annotationType.equals(RequireRangeTo.class)) {
-
-                        final Map<String, String> envaliContent = getEnvaliContent(entityClass, contentMapping,
-                                field.getName()).get(0);
-
-                        Preconditions.requireRange(getInt(entity, field),
-                                Integer.parseInt(envaliContent.get(EnvaliContentAttribute.RANGE_TO.getTag())),
-                                new InvalidValueDetectedException());
-
-                    } else if (annotationType.equals(RequireRangeFromTo.class)) {
-
-                        final Map<String, String> envaliContent = getEnvaliContent(entityClass, contentMapping,
-                                field.getName()).get(0);
-
-                        Preconditions.requireRange(getInt(entity, field),
-                                Integer.parseInt(envaliContent.get(EnvaliContentAttribute.RANGE_FROM.getTag())),
-                                Integer.parseInt(envaliContent.get(EnvaliContentAttribute.RANGE_TO.getTag())),
-                                new InvalidValueDetectedException());
-
-                    } else if (annotationType.equals(RequireStartWith.class)) {
-
-                        final Map<String, String> envaliContent = getEnvaliContent(entityClass, contentMapping,
-                                field.getName()).get(0);
-
-                        Preconditions.requireStartWith(getString(entity, field),
-                                envaliContent.get(EnvaliContentAttribute.START_WITH.getTag()),
-                                new InvalidValueDetectedException());
-
-                    } else if (annotationType.equals(RequireEndWith.class)) {
-
-                        final Map<String, String> envaliContent = getEnvaliContent(entityClass, contentMapping,
-                                field.getName()).get(0);
-
-                        Preconditions.requireEndWith(getString(entity, field),
-                                envaliContent.get(EnvaliContentAttribute.END_WITH.getTag()),
-                                new InvalidValueDetectedException());
-
-                    } else if (annotationType.equals(RequireNonEmpty.class)) {
-
-                        final Class<?> fieldDataType = field.getType();
-
-                        if (fieldDataType.isArray() || fieldDataType.equals(List.class)) {
-                            Preconditions.requireNonEmpty(Arrays.asList(field.get(entity)));
-                        } else if (fieldDataType.equals(Map.class)) {
-                            Preconditions.requireNonEmpty((Map<?, ?>) field.get(entity));
-                        } else if (fieldDataType.equals(String.class)) {
-                            Preconditions.requireNonEmpty(getString(entity, field));
-                        } else {
-                            throw new UnsupportedOperationException();
-                        }
-                    }
-                } catch (IllegalArgumentException | IllegalAccessException e) {
-                    throw new UnsupportedOperationException(e);
-                }
+                AnnotationContext.of(entity, field, annotation.annotationType()).validate();
             });
         });
-    }
-
-    /**
-     * Returns a string value from a field object.
-     *
-     * @param entity Entity object for validation
-     * @param field  Field object for validation
-     * @return A string field value
-     *
-     * @exception NullPointerException If {@code null} is passed as an argument
-     * @throws IllegalArgumentException If a different object is passed during the
-     *                                  reflection process
-     * @throws IllegalAccessException   If an attempt is made to access an area that
-     *                                  does not meet the permissions during the
-     *                                  reflection process
-     */
-    private static String getString(final ValidatableEntity entity, final Field field)
-            throws IllegalArgumentException, IllegalAccessException {
-        Preconditions.requireNonNull(entity);
-        Preconditions.requireNonNull(field);
-        return String.valueOf(field.get(entity));
-    }
-
-    /**
-     * Returns an int value from a field object.
-     *
-     * @param entity Entity object for validation
-     * @param field  Field object for validation
-     * @return An int field value
-     *
-     * @exception NullPointerException If {@code null} is passed as an argument
-     * @throws IllegalArgumentException If a different object is passed during the
-     *                                  reflection process
-     * @throws IllegalAccessException   If an attempt is made to access an area that
-     *                                  does not meet the permissions during the
-     *                                  reflection process
-     */
-    private static int getInt(final ValidatableEntity entity, final Field field)
-            throws NumberFormatException, IllegalArgumentException, IllegalAccessException {
-        Preconditions.requireNonNull(entity);
-        Preconditions.requireNonNull(field);
-        return Integer.parseInt(getString(entity, field));
-    }
-
-    /**
-     * Refer to the content file mapped to the entity object to be validated and get
-     * each expected value in {@link List} format to be used at validation.
-     *
-     * @param entityClass    Entity objects to be validated
-     * @param contentMapping Content mapping object
-     * @param fieldName      Field name for validation
-     * @return Envali's validation content
-     *
-     * @exception NullPointerException          If {@code null} is passed as an
-     *                                          argument
-     * @exception IllegalArgumentException      If {@code null} or {@code ""} string
-     *                                          is passed as an argument
-     * @exception UnsupportedOperationException If couldn't get Envali's content
-     */
-    private static List<Map<String, String>> getEnvaliContent(final Class<? extends ValidatableEntity> entityClass,
-            final ParameterMapping contentMapping, final String fieldName) {
-        Preconditions.requireNonNull(entityClass);
-        Preconditions.requireNonNull(contentMapping);
-        Preconditions.requireNonEmpty(fieldName, new IllegalArgumentException());
-
-        final List<Map<String, String>> envaliContent = ContentLoader.load(
-                entityClass.getClassLoader().getResourceAsStream(
-                        EnvaliContentRoot.ROOT.getTag() + contentMapping.content() + Extension.json()),
-                getContentAttributes(), getContentConditions(fieldName));
-
-        if (envaliContent.isEmpty()) {
-            throw new UnsupportedOperationException();
-        }
-
-        return envaliContent;
-    }
-
-    /**
-     * Returns a list containing Envali's content attributes based on the definition
-     * information for {@link EnvaliContentAttribute} .
-     *
-     * @return A list containing Envali's content attributes
-     */
-    private static List<String> getContentAttributes() {
-        return Arrays.asList(EnvaliContentAttribute.values()).stream().map(EnvaliContentAttribute::getTag)
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * Returns a map containing Envali's content conditions based on the definition
-     * information for {@link EnvaliContentCondition} .
-     *
-     * @param fieldName Field name for validation
-     * @return A map containing Envali's content conditions
-     *
-     * @exception IllegalArgumentException If {@code null} or {@code ""} string is
-     *                                     passed as an argument
-     */
-    private static Map<String, String> getContentConditions(final String fieldName) {
-        Preconditions.requireNonEmpty(fieldName, new IllegalArgumentException());
-        return Map.of(EnvaliContentCondition.VARIABLE_NAME.getTag(), fieldName);
     }
 }
