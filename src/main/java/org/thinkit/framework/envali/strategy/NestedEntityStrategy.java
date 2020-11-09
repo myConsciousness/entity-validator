0b507d2be0f956f1770759094e8d9b8fc698088f
Kato Shinya <yourdream28@gmail.com>
2020-11-09 22:36:33 +0900

    add

---
+++ src/main/java/org/thinkit/framework/envali/strategy/NestedEntityStrategy.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.framework.envali.strategy;
+
+import java.lang.reflect.Field;
+
+import org.thinkit.framework.envali.annotation.NestedEntity;
+import org.thinkit.framework.envali.entity.ValidatableEntity;
+
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.ToString;
+
+/**
+ * A strategy to perform validation process for {@link NestedEntity} annotation.
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@ToString
+@EqualsAndHashCode(callSuper = false)
+final class NestedEntityStrategy extends ValidationStrategy {
+
+    private NestedEntityStrategy(@NonNull ValidatableEntity entity, @NonNull Field field) {
+        super(entity, field);
+    }
+
+    public static ValidationStrategy of(@NonNull ValidatableEntity entity, @NonNull Field field) {
+        return new NestedEntityStrategy(entity, field);
+    }
+
+    @Override
+    public void validate() {
+
+    }
+}
